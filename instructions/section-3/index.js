4d2e70043ddc636b39b76d043c60c6b18b3a81cb
BeccaL <103158276+PixelBayBuilds@users.noreply.github.com>
2023-08-08 22:04:04 -0400

    updated
    Updated skills section

--- instructions/section-3/index.js
+++ instructions/section-3/index.js
@@ -4,3 +4,12 @@ const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
 copyright.innerHTML = "Rebecca L " + thisYear;
 footer.appendChild(copyright);
+/*This is the skills section*/
+const skills = ["UX/UI", "Digital Design", "Figma"];
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.querySelector("ul");
+for (i = 0; i < skills.length; i++) {
+    const skill = document.createElement("li");
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
